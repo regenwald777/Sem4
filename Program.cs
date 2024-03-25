7ebf73dc4c51e0e44d5a8fa0fd43fa329dc507ce
goliathus <goliathus@mail.ru>
2024-03-25 13:11:13 +0500

    First zad

--- Program.cs
+++ Program.cs
@@ -14,6 +14,27 @@ void ShowArray(int [] array){
     Console.WriteLine();
 }
 
+int CountofPrimes(int [] array){
+    int count = 0;
+    foreach(int el in array){
+            if(IsPrime(el)){
+                count++;
+            }
+    }
+    return count;
+}
+
+bool IsPrime(int num){
+        double limit = Math.Sqrt(num);
+        for(int i = 2; i <= limit;i++){
+            if(num%i==0){
+                return false;
+            }
+            
+        }
+        return true;
+}
+
 Console.WriteLine("Введите минимальное число в массиве");
 int min = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите максимальное число в массиве");
@@ -21,5 +42,13 @@ int max = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите размер массива");
 int size = Convert.ToInt32(Console.ReadLine());
 
+//задать массив заполненный случайными числами и определить количество простых чисел в массиве.
+//Примеры
+//[1 3 4 19 3] =>2
+//[4 3 4 1 9 5 21 13] =>3
+
+
 int [] array = CreateRandomArray(min, max, size);
 ShowArray(array);
+int count = CountofPrimes(array);
+Console.WriteLine($"Количество простых чисел в массиве равно - {count}");
