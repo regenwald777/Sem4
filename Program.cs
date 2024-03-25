cfb06348c4471bcb4c82ea7fb607621530219a84
goliathus <goliathus@mail.ru>
2024-03-25 12:12:24 +0500

    intro

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+int [] CreateRandomArray(int min, int max, int size){
+    int [] array = new int [size];
+    for(int i=0; i<size; i++){
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void ShowArray(int [] array){
+    foreach(int item in array){
+        Console.Write(item+" ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите минимальное число в массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное число в массиве");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int [] array = CreateRandomArray(min, max, size);
+ShowArray(array);
